529c4e188926ae64c2897acd49d52792e3848b1a
Hanbiubiu <hanjianxin2018@163.com>
2022-11-16 20:42:45 +0800

    main

--- test/main.cpp
+++ test/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include <unordered_map>
+
 #include<iterator>
 using namespace std;
 int main()
 {
-    unordered_map<int, int> Hash;
-    Hash.insert({ {1,3},{2,4} });
-    unordered_map<int, int>::iterator it;
-    cout << it->first << endl;
-  
+    char buffer[]={"hello"};
+    char *end=buffer+4;
+    char a=(*end);
+    cout<<int(a);
+
 }
+    
